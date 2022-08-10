231e9bd219ae20503184022a3ac65285feb3baef
wprada <williampmercay@gmail.com>
2022-08-10 13:11:28 -0500

    Deploy

---
+++ src/App/index.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import { TodoProvider } from '../TodoContext';
+import { AppUI } from './AppUI';
+
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar el cursso de intro a React', completed: false },
+//   { text: 'Llorar con la llorona', completed: true },
+//   { text: 'LALALALAA', completed: false },
+// ];
+
+function App() {
+  return (
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
+  );
+}
+
+export default App;
